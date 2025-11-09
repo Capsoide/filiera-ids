ee4b8965d1e7029e8437ffe739dc2b78890a08ab
iobbins <mattiaiobbi@gmail.com>
2025-11-09 12:00:34 +0100

    Aggiunta modello RichiestaRuolo

---
+++ src/main/java/it/unicam/cs/ids/filieraids/model/RichiestaRuolo.java
@@ -0,0 +1,75 @@
+package it.unicam.cs.ids.filieraids.model;
+
+import jakarta.persistence.*;
+import org.springframework.lang.Nullable;
+
+import java.util.*;
+
+@Entity
+@Table(name="richiesta_ruolo")
+public class RichiestaRuolo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name="attore_id", nullable=false, unique=true)
+    private Attore attoreRichiedente;
+
+    @Enumerated(EnumType.STRING)
+    private Conferma stato;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "richiesta_ruoli_richiesti", joinColumns = @JoinColumn(name = "richiesta_id"))
+    @Enumerated(EnumType.STRING)
+    @Column(name = "ruolo")
+    private Set<Ruolo> ruoliRichiesti = new HashSet<>();
+
+    private String motivazione;
+
+    public RichiestaRuolo() {}
+
+    public RichiestaRuolo(Attore attore, Set<Ruolo> ruoli, String motivazione, Conferma stato) {
+        this.attoreRichiedente = attore;
+        this.ruoliRichiesti = ruoli;
+        this.motivazione = motivazione;
+        this.stato = Conferma.ATTESA;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Attore getAttoreRichiedente() {
+        return attoreRichiedente;
+    }
+
+    public void setAttoreRichiedente(Attore attoreRichiedente) {
+        this.attoreRichiedente = attoreRichiedente;
+    }
+
+    public Conferma getStato() {
+        return stato;
+    }
+
+    public void setStato(Conferma stato) {
+        this.stato = stato;
+    }
+
+    public Set<Ruolo> getRuoliRichiesti() {
+        return ruoliRichiesti;
+    }
+
+    public void setRuoliRichiesti(Set<Ruolo> ruoliRichiesti) {
+        this.ruoliRichiesti = ruoliRichiesti;
+    }
+
+    public String getMotivazione() {
+        return motivazione;
+    }
+
+    public void setMotivazione(String motivazione) {
+        this.motivazione = motivazione;
+    }
+}

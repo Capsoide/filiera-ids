cda841cf65bb3bf9fd8dbb5db570d80304323af6
tinafrolls <martina.frolla@studenti.unicam.it>
2025-11-25 20:08:47 +0100

    Modifiche alla classe Indirizzo per OSM

--- src/main/java/it/unicam/cs/ids/filieraids/model/Indirizzo.java
+++ src/main/java/it/unicam/cs/ids/filieraids/model/Indirizzo.java
@@ -1,5 +1,8 @@
 package it.unicam.cs.ids.filieraids.model;
 import jakarta.persistence.Embeddable;
+import jakarta.persistence.Transient;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
 
 @Embeddable
 public class Indirizzo {
@@ -9,16 +12,27 @@ public class Indirizzo {
     private String comune;
     private String CAP;
     private String regione;
+    @DecimalMin(value = "-90", message = "Latitudine non valida.")
+    @DecimalMax(value = "90", message = "Latitudine non valida.")
+    private Double latitudine;
+    @DecimalMin(value = "-180", message = "Longitudine non valida.")
+    @DecimalMax(value = "180", message = "Longitudine non valida.")
+    private Double longitudine;
 
     public Indirizzo(){}
 
-    public Indirizzo(String via, String numCivico, String comune, String CAP, String regione) {
+    public Indirizzo(String via, String numCivico, String comune, String CAP, String regione, Double latitudine, Double longitudine) {
         this.via = via;
         this.numCivico = numCivico;
         this.comune = comune;
         this.CAP = CAP;
         this.regione = regione;
     }
+
+    public Indirizzo(String via, String numCivico, String comune, String CAP, String regione) {
+        this(via, numCivico, comune, CAP, regione, null, null);
+    }
+
 
     public String getVia() {
         return via;
@@ -60,13 +74,24 @@ public class Indirizzo {
         this.regione = regione;
     }
 
+    public Double getLatitudine(){ return latitudine; }
+
+    public void setLatitudine(double latitudine) { this.latitudine = latitudine; }
+
+    public Double getLongitudine(){ return longitudine; }
+
+    public void setLongitudine(double longitudine) { this.longitudine = longitudine; }
+
+    @Transient
     public String getIndirizzoCompleto(){
         return via + " " + numCivico + ", " + CAP + " " + comune + " (" + regione + ")";
     }
 
     @Override
     public String toString() {
-        return getIndirizzoCompleto();
+        String coordinate = (latitudine != null && longitudine != null)
+                ? (" [GPS: " + latitudine + ", " + longitudine + "]") : " ";
+        return getIndirizzoCompleto() + coordinate;
     }
 
 }

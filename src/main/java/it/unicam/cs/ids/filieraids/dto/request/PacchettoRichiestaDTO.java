e606234cc6f3696abb27030d4baee7ee09e8495d
iobbins <mattiaiobbi@gmail.com>
2025-11-30 15:02:35 +0100

    Sistemazione annotazioni PacchettoRichiestaDTO

--- src/main/java/it/unicam/cs/ids/filieraids/dto/request/PacchettoRichiestaDTO.java
+++ src/main/java/it/unicam/cs/ids/filieraids/dto/request/PacchettoRichiestaDTO.java
@@ -1,20 +1,23 @@
 package it.unicam.cs.ids.filieraids.dto.request;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
+
 import java.util.List;
 
 public record PacchettoRichiestaDTO(
 
-        @NotBlank
+        @NotBlank(message="Il nome del pacchetto è obbligatorio")
+        @Size(max = 100, message = "Il nome non può superare i 100 caratteri")
         String nome,
 
-        @NotBlank
+        @NotBlank(message = "La descrizione è obbligatoria")
+        @Size(max = 500, message = "La descrizione non può superare i 500 caratteri")
         String descrizione,
 
-        @NotNull
+        @NotNull(message = "Il prezzo è obbligatorio")
+        @DecimalMin(value = "0.01", message = "Il prezzo deve essere maggiore di zero")
         Double prezzo,
 
-        @NotNull
+        @NotEmpty(message = "La lista dei prodotti non può essere vuota")
         List<PacchettoItemRichiestaDTO> items
 ) {}

7ee5349cc73c76f215a6f7abdc09c58b89bbd18d
iobbins <mattiaiobbi@gmail.com>
2025-11-30 15:48:22 +0100

    Sistemazione annotazioni PacchettoItemRichiestaDTO

--- src/main/java/it/unicam/cs/ids/filieraids/dto/request/PacchettoItemRichiestaDTO.java
+++ src/main/java/it/unicam/cs/ids/filieraids/dto/request/PacchettoItemRichiestaDTO.java
@@ -1,6 +1,13 @@
 package it.unicam.cs.ids.filieraids.dto.request;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+
 public record PacchettoItemRichiestaDTO(
+        @NotBlank(message = "L'id del prodotto è obbligatorio")
         Long prodottoId,
+
+        @NotBlank(message = "La quantità del prodotto è obbligatoria")
+        @Min(value = 1, message = "La quantita non può essere zero")
         int quantita
 ) {}
